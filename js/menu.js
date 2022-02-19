3b568a11867e369932f59e178a528544cd22d363
Vitalii Tieliehin <vantaglist@gmail.com>
2022-02-19 21:16:17 +0100

    mobile menu is done

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,14 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector('.open-mob-menu'),
-    closeMenuBtn: document.querySelector('.close-mob-menu'),
-    menu: document.querySelector('.mobile-menu'),
-  };
-
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
-  function toggleMenu() {
-    // document.body.classList.toggle("menu-open")
-    refs.menu.classList.toggle('is-hidden');
-  }
-})();
-
-
-
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+  
+    menuBtnRef.addEventListener("click", () => {
+      const expanded =
+        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  
+      menuBtnRef.classList.toggle("is-open");
+      menuBtnRef.setAttribute("aria-expanded", !expanded);
+      document.body.classList.toggle("modal-open")
+      mobileMenuRef.classList.toggle("is-open");
+    });
+  })();   
