91f2ddc6c4c576c58e036bd1bc65f23601b7a1fa
Vitalii Tieliehin <vantaglist@gmail.com>
2022-02-19 20:01:57 +0100

    Mobile menu all hover focus done

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,16 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector(".open-mob-menu"),
-    closeMenuBtn: document.querySelector(".close-mob-menu"),
-    menu: document.querySelector(".mobile-menu"),
+    openMenuBtn: document.querySelector('.open-mob-menu'),
+    closeMenuBtn: document.querySelector('.close-mob-menu'),
+    menu: document.querySelector('.mobile-menu'),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    document.body.classList.toggle("menu-open")
-    refs.menu.classList.toggle("is-hidden");
+    // document.body.classList.toggle("menu-open")
+    refs.menu.classList.toggle('is-hidden');
   }
 })();
 
